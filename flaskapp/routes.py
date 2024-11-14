4ff739de50bea8b2dbd2ea2b636e760ac934431d
icpinto <icpinto76.ic@gmail.com>
2024-11-14 16:32:10 +0000

    Fetch all commits

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -81,6 +81,7 @@ def save_metrics_to_db(metrics, model_version, dataset_version):
 #test github workflow
 #test github workflow
 #test github workflow
+#test github workflow
 
 # Models route
 @models_blueprint.route('/models', methods=['GET'])
